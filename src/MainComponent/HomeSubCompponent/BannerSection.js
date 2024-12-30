7eea2786aa09a3f28280d82b9efb21be10d85eab
rajas <rajaskadge@gmail.com>
2024-12-30 14:11:13 +0530

    BannerSection btn
    BannerSection Btn style changed

--- src/MainComponent/HomeSubCompponent/BannerSection.js
+++ src/MainComponent/HomeSubCompponent/BannerSection.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
+import './BannerSliderBtn.css'
 import { BASE_URL, IMG_URL } from '../../AdminComponent/BaseUrl';
 import { Link } from 'react-router-dom';
 
